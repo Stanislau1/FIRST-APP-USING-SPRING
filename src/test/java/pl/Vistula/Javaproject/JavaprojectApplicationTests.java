db0ead43f7abfb7a8a9aaf0ae549cc025463e9db
Стас Волосюк <stas@MacBook-Pro-Stas.local>
2025-01-20 22:38:51 +0100

    Initial commit

---
+++ src/test/java/pl/Vistula/Javaproject/JavaprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.Vistula.Javaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
